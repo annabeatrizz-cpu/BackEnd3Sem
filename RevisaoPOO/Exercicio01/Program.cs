6bec856e0abcb204184a258874d19bd26e90e9d1
ANNA BEATRIZ ARENAS HENKEL <56400424865@info.local>
2026-02-13 13:58:15 -0300

    RevisaoPOO

---
+++ RevisaoPOO/Exercicio01/Program.cs
@@ -0,0 +1,14 @@
+﻿using RevisaoPOO;
+
+Pessoa Anna = new Pessoa();
+
+Anna.Nome = "Anna Beatriz";
+Anna.Idade = 16;
+
+
+// Console.WriteLine($"Nome: {Anna.Nome}");
+// Console.WriteLine($"Idade da {Anna.Nome}: {Anna.Idade} anos");
+
+//Exercicio 2
+
+Anna.ExibirDados();
